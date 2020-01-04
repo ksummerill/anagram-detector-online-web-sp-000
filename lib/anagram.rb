170986ad3e06493f0ad2775dd99f6940219dcc31
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-04 16:24:13 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,15 +5,25 @@ class Anagram
   attr_accessor :word
 
   def initialize(word)
-    @word = word
+    # for each word initialized, split into array of letters for comparison in match method
+    # ex: "diaper" => ["d", "i", "a", "p", "e", "r"]
+    @word = word.split("")
   end
 
   result = nil
   # take in array of possible anagrams - diaper: (hello world zombies pants dipper)
+  # iterate over array of anagrams, compare each word of the array to the word that the Anagram class is initialized with
+  # determine if the word in the array is composed of the same letters - split word into array of letters.
   # return all matches in the array.
   # if no matches exist, return an empty array
   def match(array)
-
+    array.detect do |word|
+      # split word into arry of letters
+      if word.split("") == @word
+        return word
+      end    
+      
+    end
   end
 
 
