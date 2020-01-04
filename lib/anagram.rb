73578cebd5f5b6b3ab180003a4b4ec30b876c17e
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-04 16:27:46 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -17,7 +17,7 @@ class Anagram
   # return all matches in the array.
   # if no matches exist, return an empty array
   def match(array)
-    array.detect do |word|
+    array.find_all do |word|
       # split word into arry of letters
       if word.split("").sort == @word.sort
         word
