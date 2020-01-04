919fd2a8d1d8baf35d88b3cdf59af4e7f7549938
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-04 16:12:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,13 @@ class Anagram
     @word = word
   end
 
-  
+  result = nil
+  # take in array of possible anagrams - diaper: (hello world zombies pants dipper)
+  # return all matches in the array.
+  # if no matches exist, return an empty array
+  def match(array)
+
+  end
 
 
 end
