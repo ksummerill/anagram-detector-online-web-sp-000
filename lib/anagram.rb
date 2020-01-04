cc71a44f3b26d9c4780575e98d659f0295a04a61
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-04 16:06:09 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+# Build a class with an initialize method and use an attr_accessor
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  
+
+
+end
